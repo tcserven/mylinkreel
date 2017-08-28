bd9af836ba039067aac795815db95d29e768243b
Thomas-Laptop <thomas.cservenyi@gmail.com>
2017-08-28 16:34:36 -0400

    fixing homepage layout aug 28

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express'),
-	  path = require('path'),
-	  bodyParser = require('body-parser');
+	path = require('path'),
+	bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-var app = express();
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // serve up the public folder
-app.use(express.static(path.join(__dirname, "/app/public/")));
+app.use(express.static(path.join(__dirname, '/app/public/')));
 
 
-app.get("/", function(req, res) {    
-    res.render('index');    
+app.get('/', function (req, res) {    
+    res.render('index');  
 });
 
-app.get("/contact", function(req, res) {    
+app.get('/contact', function (req, res) {    
     res.render('contact');    
 });
 
-app.listen(port, function() {
-	console.log("server has started on " + port);
+app.listen(port, function () {
+	console.log('server has started on ' + port);
 });
